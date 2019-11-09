fff8726cc478219408c94922481ed3d6254944d3
Cicko <rudolfcicko2016@gmail.com>
2019-11-09 17:44:13 +0100

    Form send data

--- dev/server.js
+++ dev/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded());
+
 app.post('/start', (req, res) => {
-    res.send('ok hahah');
+    const { name, numClients } = req.body;
+    console.log('Name:', name);
+    console.log('Num clients:', numClients);
+    res.send('alter button title');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,14 +5,22 @@
 import React from 'react';
 import axios from 'axios';
 import { Link, Redirect } from "react-router-dom";
+import { Form, Input, Select, Button } from 'semantic-ui-react'
+import Background from '../assets/images/background.jpg';
 
 class HomePage extends React.Component {
     state = {
         chefInformed: false,
+        numClients: 1,
+        name: '',
     };
 
     informChef = () => {
-        axios.post('/start').then(data => {
+        const { numClients, name } = this.state;
+        axios.post('/start', {
+            numClients,
+            name,
+        }).then(data => {
             console.log(data);
             this.setState({
                 chefInformed: true,
@@ -22,13 +30,35 @@ class HomePage extends React.Component {
         });
     };
 
+    renderForm = () => <Form style={{ justifyContent: 'center', display: 'flex', flexDirection: 'column', padding: 20,  top: '22%' }}>
+        <Form.Field>
+            <label>Restaurant's name</label>
+            <Input value={this.state.name} onChange={(e) => { this.setState({ name: e.target.value })}}/>
+        </Form.Field>
+        <Form.Field>
+            <label>Nº persons</label>
+            <Select
+                placeholder='Last Name'
+                options={
+                [1,2,3,4,5,6,7,8,9].map((v) => ({
+                    key: v, value: v, text: v + ' persons',
+                }))}
+                value={this.state.numClients}
+                onChange={(_e, data) => {
+                    this.setState({ numClients: data.value });
+                }}
+            />
+        </Form.Field>
+        <Button onClick={this.informChef} type='submit'>Submit</Button>
+    </Form>;
+
     render() {
         if (this.state.chefInformed) {
             return <Redirect to='/map2' />
         }
-        return (
-            <button onClick={this.informChef}>Brauche Essen</button>
-        );
+        return <div style={{ backgroundColor: '#2196F3', height: '100%'}}>
+                {this.renderForm()}
+            </div>;
     }
 }
 
